0f2ae72a1d968d9408cdcea0a526af78923754e3
ASHAR ALI <111843979+ASHAR797@users.noreply.github.com>
2024-05-05 17:11:07 +0500

    Updated files

---
+++ app.js
@@ -0,0 +1,9 @@
+const navBar = document.querySelector('.navigation-bar');
+
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 0) {
+    navBar.classList.add('scrolled');
+  } else {
+    navBar.classList.remove('scrolled');
+  }
+});
