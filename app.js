f625f51e37ede0472889940a26fde7b5caef8ec2
ASHAR ALI <111843979+ASHAR797@users.noreply.github.com>
2024-05-07 02:47:28 +0500

    Updated files

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const navBar = document.querySelector('.navigation-bar');
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   if (window.scrollY > 0) {
     navBar.classList.add('scrolled');
   } else {
